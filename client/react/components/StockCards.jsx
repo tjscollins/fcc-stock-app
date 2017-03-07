7b81ec1f2a3b283f5354815934e6d15d285eb44e
Tyler Collins <tjscollins@gmail.com>
2017-03-07 18:18:11 +1000

    Implemented mock methods for StockCards and basic styling

--- client/react/components/StockCards.jsx
+++ client/react/components/StockCards.jsx
@@ -7,9 +7,36 @@ export class StockCards extends React.Component {
   constructor() {
     super();
   }
+  createCard(title, text) {
+    return (
+      <div className='info-card'>
+        <h1>{title}</h1>
+        <p>{text}</p>
+      </div>
+    );
+  }
+  createStockCards() {
+    // get loaded stocks from state and create info cards
+  }
+  submit(e) {
+    e.preventDefault();
+    // Make API Call for new stock and update state with result
+  }
   render() {
     return (
-      <div />
+      <div className='stock-cards'>
+        {this.createStockCards()}
+        <div className='search-card'>
+
+          <form onSubmit={this.submit}>
+            <label>
+              Search for stocks:
+            </label>
+            <input type='text' />
+            <button type='submit'>Add</button>
+          </form>
+        </div>
+      </div>
     );
   }
 }
